2f682cf1098468038aec18899c406eda89f590a9
Sohaib <sohaib.al.khan@gmail.com>
2022-11-14 09:47:26 -0500

    :)

--- Ansible_Vault/run_playbook.sh
+++ Ansible_Vault/run_playbook.sh
@@ -2,25 +2,21 @@
 
 # NOTE: Replace the password file path when runing this script
 host_pass_file=.key
+vault_key_file=.vault_file
 
 # Ensure file is already encrypted before running the full script 
-ansible-vault encrypt --vault-password-file ../.ansible_vault_key  $host_pass_file 2>/dev/null
-ansible-vault decrypt --vault-password-file ../.ansible_vault_key $host_pass_file
+ansible-vault encrypt --vault-password-file $vault_key_file $host_pass_file 2>/dev/null
+ansible-vault decrypt --vault-password-file $vault_key_file $host_pass_file
 
 # If secret is decrypted then run playbook
 if [ $? -eq 0 ]
 then
-ansible-playbook playbook.yaml -i host_list  --vault-password-file ../.ansible_vault_key --become-password-file $host_pass_file
+ansible-playbook playbook.yaml -i host_list  --vault-password-file $vault_key_file --become-password-file $host_pass_file
     else
    echo 'could not decrypt the secret'
-   fi
+    fi
 
 sleep 5s
 
 # Encypt the secret again
-if [ $? -eq 0 ]
-then
-    ansible-vault encrypt --vault-password-file ../.ansible_vault_key  $host_pass_file
-    else
-   echo 'could not encrypt the secret'
-   fi
+ansible-vault encrypt --vault-password-file $vault_key_file  $host_pass_file
